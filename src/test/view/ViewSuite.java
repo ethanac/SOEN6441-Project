5590497a3d2de1ca2f45d13f856f4906a04b8d43
Ethan <haoz86@gmail.com>
2016-02-17 01:57:21 -0500

    Modify the ViewSuite in test.view package

--- src/test/view/ViewSuite.java
+++ src/test/view/ViewSuite.java
@@ -1,14 +1,14 @@
 package test.view;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-public class ViewSuite {
+//import org.junit.runner.RunWith;
+//import org.junit.runners.Suite;
+//import org.junit.runners.Suite.SuiteClasses;
+//
+//public class ViewSuite {
 //	@RunWith(Suite.class)
 //	@SuiteClasses({ViewTestGameControlPanel.class,
 //					ViewTestMapControlPanel.class})
 //	public class SuiteCommon {
 //
 //	}
-}
+//}
