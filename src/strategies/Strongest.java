c1b2b66d8da99f8b2d355c0c426266e7e60011cf
MengYao89 <yaomenghncn@gmail.com>
2016-03-16 14:33:25 -0400

    4 different types critters
    strong，weak， closet，farthest

--- src/strategies/Strongest.java
+++ src/strategies/Strongest.java
@@ -1,5 +1,44 @@
 package strategies;
 
-public class Strongest {
 
+import java.util.ArrayList;
+
+import models.Critter;
+import models.Tower;
+
+/**
+ *
+ * finds the strongest enemy
+ * 
+ */
+public class Strongest implements IStrategy {
+
+    /**
+     *
+     * @param tower
+     * @param g1
+     * @return
+     */
+    @Override
+	public Critter findTarget(Tower tower, ArrayList<Critter> g1) {
+    	
+		Critter strongest_enemy= null;   
+        double max_health = 0;
+        int count = g1.size(); 			 		 // The number of critters currently within range
+        for (int i = 0; i < count; i++) {
+            Critter enemy = g1.get(i); 			
+            double health = (g1.get(i).getHitPoints());		     	 // The health of critter
+            if (health > max_health)
+            {
+            max_health = health;
+            strongest_enemy=enemy;
+            }
+        }
+        
+       return(strongest_enemy);				 	 // Tower attacks this critter
+		
+	}
+	public String toString(){
+		return "Strongest";
+	}
 }
--- src/strategies/Weakest.java
+++ src/strategies/Weakest.java
@@ -1,5 +1,47 @@
 package strategies;
 
-public class Weakest {
+
+import java.util.ArrayList;
+
+import models.Critter;
+import models.Tower;
+
+/**
+ *
+ * finds the weakest enemy
+ * 
+ */
+public class Weakest implements IStrategy {
+
+    /**
+     *
+     * @param tower
+     * @param g1
+     * @return
+     */
+    @Override
+	public Critter findTarget(Tower tower, ArrayList<Critter> g1) {
+	
+		Critter weakest_enemy= null;   
+        double min_health = 10000.0; //arbitrary number
+        int count = g1.size(); 			 		 // The number of critters currently within range
+        for (int i = 0; i < count; i++) {
+            Critter enemy = g1.get(i); 			
+            double health = (g1.get(i).getHitPoints());	// The health of critter
+            
+            if (health < min_health)
+            {
+            min_health = health;
+            weakest_enemy=enemy;
+            }
+        }
+        
+       // System.out.println(weakest_enemy);
+       return(weakest_enemy);				 	 // Tower attacks this critter
+		
+	}
+	public String toString(){
+		return "Weakest";
+	}
 
 }
