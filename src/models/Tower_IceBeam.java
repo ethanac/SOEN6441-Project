3d8e48b780c3e020aadc0d9cf9cd5642458de64f
Teddy <ee08b0081@163.com>
2016-03-15 20:48:24 -0400

    Refine the attributes of fire, ice and spread tower
    Enable upgrading special effects of each kind of tower

--- src/models/Tower_IceBeam.java
+++ src/models/Tower_IceBeam.java
@@ -2,62 +2,72 @@ package models;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 /**
  * This class defines the IceBeam tower. 
  * It extends the Tower class.
  * 
  * @author HaoZhang
- * @version v1.0.0
+ * @version 2.0.0
  */
-public class Tower_IceBeam extends Tower{
-	
+public class Tower_IceBeam extends Tower {
+	//properties for the ice beam tower
 	static int buyCost = 150;
-	
-	/**
-	 * This is the constructor.
-	 * 
-	 * @param n A string as the name of the tower.
-	 * @param p A Point object as the position of the tower.
-	 * 
-	 */
-	public Tower_IceBeam(String n, Point p) {
-		super(n, p);
-		
+	double slowFactor;
+	int slowTime;
+    /**
+     * Constructor of IceBeam tower defines attributes of this tower type
+     *
+     * @param n
+     * @param p
+     * @param crittersOnMap
+     */
+    public Tower_IceBeam(String n, Point p, ArrayList<Critter> crittersOnMap) {
+		super(n, p, crittersOnMap);
 		//these variables are all explicitly written as all laser towers will have the same starting stats
-				damage = 0.25;
-				rateOfFire = 10;
-				range = 100;
-				sellPrice = 75;
-				upCost = 350;
-				tColor = Color.WHITE;
-				shotColor = Color.WHITE;
-				buyCost = 150;
-				
+		damage = 0.25;
+		rateOfFire = 3;
+		range = 100;
+		sellPrice = 75;
+		upCost = 350;
+		slowFactor = 0.5;
+		tColor = Color.WHITE;
+		shotColor = Color.WHITE;
+		buyCost = 150;
+		slowTime = 50;
 	}
 	
 	/**
-	 * The method to get the price of a tower you want to buy.
+	 * Shoots the critter and slowing down the move speed of critters
 	 * 
-	 * @return The cost to buy a IceBeam tower.
+	 * @see models.Tower#shootTarget(models.Critter, java.awt.Graphics)
+	 * 
 	 */
-	public static int getBuyPrice() {
+    protected void shootTarget(Critter target, Graphics g){
+		super.shootTarget(target, g);
+		target.slowCritter(this.slowFactor, this.slowTime);
+	}
+	
+    /**
+     * Getter of IceBeam tower tower purchasing price
+     *
+     * @return buyCost
+     */
+    public static int getBuyPrice(){	
 		return buyCost;
 	}
-	
-	/**
-	 * Upgrade the tower if its current level is under the maximum level.
-	 */
-	public void upgradeTower(){
+    /**
+     * This upgrades the tower and the slow down capability
+     * 
+     * @see models.Tower#upgradeTower()
+     */
+    public void upgradeTower(){
     	if(level < MAXTOWERLEVEL){
 	    	super.upgradeTower();
+			this.slowTime = (this.slowTime + 20);
+			this.slowFactor = this.slowFactor + 0.1;
     	}
     }
-	
-	@Override
-	public void updateAndDraw(Graphics g) {
-		this.drawTower(g);
-		
-	}
 
 }
--- src/models/Tower_SpreadShot.java
+++ src/models/Tower_SpreadShot.java
@@ -2,46 +2,79 @@ package models;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 /**
- * This class defines the Laser tower. 
- * It extends the SpreadShot class.
+ * This class defines the SpreadShot tower. 
+ * It extends the tower class.
  * 
  * @author HaoZhang
- * @version v1.0.0
+ * @version 2.0.0
  */
-public class Tower_SpreadShot extends Tower{
-
+public class Tower_SpreadShot extends Tower {
 	private int amountOfTargets = 3;
 	static int buyCost = 900;
-	
-	public Tower_SpreadShot(String n, Point p) {
-		super(n, p);
-		
+
+    /**
+     * Constructor of SpreadShot Tower defines attributes of this tower type
+     *
+     * @param n
+     * @param p
+     * @param crittersOnMap
+     */
+    public Tower_SpreadShot(String n, Point p, ArrayList<Critter> crittersOnMap) {
+		super(n, p, crittersOnMap);
 		//these variables are all explicitly written as all laser towers will have the same starting stats
-				damage = 0.8;
-				rateOfFire = 10;
-				range = 200;
-				sellPrice = 400;
-				upCost = 2000;
-				tColor = Color.YELLOW;
-				shotColor = Color.YELLOW;
-				buyCost = 900;
+		damage = 0.8;
+		rateOfFire = 10;
+		range = 200;
+		sellPrice = 400;
+		upCost = 2000;
+		tColor = Color.YELLOW;
+		shotColor = Color.YELLOW;
+		buyCost = 900;
 	}
+	
+	
+    /**
+     * Overrides the updateAndDraw method in the tower class to shoot more than one critter.
+     *
+     * @see models.Tower#updateAndDraw(java.awt.Graphics)
+     */
+    public void updateAndDraw(Graphics g){	
+    	//initialize in range critters and targets
+		ArrayList<Critter> inRangeC = new ArrayList<Critter>();
+		ArrayList<Critter> targets = new ArrayList<Critter>();
 
-	/**
-	 * The method to get the price of a tower you want to buy.
-	 * 
-	 * @return The cost to buy a SpreadShot tower.
-	 */
-	public static int getBuyPrice(){	
-		return buyCost;
-	}
-
-	@Override
-	public void updateAndDraw(Graphics g) {
+		//find our critters in range
+		inRangeC = this.findCrittersInRange(potentialCrittersInRange);
+		//select the target critter
+		Critter targetedCritter = this.selectTarget(this, inRangeC);
+		//remove this target from the list and find the next target
+		for(int i = 0; i < amountOfTargets; i++){
+			targets.add(targetedCritter);
+			inRangeC.remove(targetedCritter);
+			targetedCritter = this.selectTarget(this, inRangeC);
+		}
+		//shoot all of the targets
+		if(!targets.isEmpty()){
+			for(Critter c : targets){
+				if(c != null){
+					this.shootTarget(c, g);
+				}
+			}
+		}
+		//draw our tower
 		this.drawTower(g);
-		
+	}
+	
+    /**
+     * Getter of SpreadShot tower purchasing price
+     * 
+     * @return buyCost
+     */
+    public static int getBuyPrice(){	
+		return buyCost;
 	}
 
 }
